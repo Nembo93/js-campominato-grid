874ff31a7158b639118899d882ad88fac3da5671
nembo93 <francesco.c193@libero.it>
2022-11-03 14:56:57 +0100

    Aggiunge funzione per creare griglia all'interno di una condizione che ne stabilisce il numero di celle in base al livello di difficoltà

--- js/main.js
+++ js/main.js
@@ -3,37 +3,67 @@
 // Aggiungere alla cella la possibilità di colorarsi con un click
 
 const boardContainer = document.querySelector(`.container`);
-
 const gameLevel = document.getElementById(`level`);
-
 
 const playStart = document.getElementById(`play_start`);
 playStart.addEventListener(`click`, function(){
 
+boardContainer.innerHTML = ``;
     // Definire variabili per creazione celle
     let easyGameCell = 100;
     let mediumGameCell = 81;
-    let hardgameCell = 49;
+    let hardGameCell = 49;
 
     if (gameLevel.value === `facile`){
         console.log(`facile`);
+        for( i = 1; i <= easyGameCell; i++){
+            const boardCell = document.createElement(`div`);
+            boardCell.classList.add(`cell`);
+            boardCell.innerHTML = i;
+            boardContainer.append(boardCell);
+            boardCell.addEventListener(`click`, function(){
+                console.log(this.innerHTML);
+                this.classList.add(`clicked`);
+            })
+        }
     } else if (gameLevel.value === `medio`){
         console.log(`medio`);
+        for( i = 1; i <= mediumGameCell; i++){
+            const boardCell = document.createElement(`div`);
+            boardCell.classList.add(`cell`);
+            boardCell.innerHTML = i;
+            boardContainer.append(boardCell);
+            boardCell.addEventListener(`click`, function(){
+                console.log(this.innerHTML);
+                this.classList.add(`clicked`);
+            })
+        }
     } else{
         console.log(`difficile`);
+        for( i = 1; i <= hardGameCell; i++){
+            const boardCell = document.createElement(`div`);
+            boardCell.classList.add(`cell`);
+            boardCell.innerHTML = i;
+            boardContainer.append(boardCell);
+            boardCell.addEventListener(`click`, function(){
+                console.log(this.innerHTML);
+                this.classList.add(`clicked`);
+            })
+        }
     }
 
-    for( i = 1; i <= easyGameCell; i++){
-        const boardCell = document.createElement(`div`);
-        boardCell.classList.add(`cell`);
-        boardCell.innerHTML = i;
-        boardContainer.append(boardCell);
-        boardCell.addEventListener(`click`, function(){
-            console.log(this.innerHTML);
-            this.classList.add(`clicked`);
-        })
-    }
+    
 })
 
 
 
+// for( i = 1; i <= easyGameCell; i++){
+//     const boardCell = document.createElement(`div`);
+//     boardCell.classList.add(`cell`);
+//     boardCell.innerHTML = i;
+//     boardContainer.append(boardCell);
+//     boardCell.addEventListener(`click`, function(){
+//         console.log(this.innerHTML);
+//         this.classList.add(`clicked`);
+//     })
+// }
